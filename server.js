a64e50d4cf927bcad2242acfb75777e8827be129
prabodhalahiru <prabodhalahiru119@gmail.com>
2021-10-04 14:16:43 +0530

    comment added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-// const mongoose = require('mongoose')
+// const mongoose = require('mongoose') 2
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
